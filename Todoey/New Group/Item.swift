e6dd44d22e36ff76f1bd7db4f7824c69b3d07d89
Guzman Vilanova <guzman.vilanova@gmail.com>
2018-10-13 01:01:07 -0300

    MVC

---
+++ Todoey/New Group/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Data.swift
+//  Todoey
+//
+//  Created by Guzmán Vilanova on 10/13/18.
+//  Copyright © 2018 Guzmán Vilanova. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+    
+}
